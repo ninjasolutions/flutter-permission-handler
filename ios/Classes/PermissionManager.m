6fbcdcd26a5896c41dc1a7f7cc6aff483fa3ebb5
Genert Org <orggenert@gmail.com>
2019-09-26 11:36:15 +0300

    FA-74: onboarding permissions for the app

--- ios/Classes/PermissionManager.m
+++ ios/Classes/PermissionManager.m
@@ -75,8 +75,6 @@
 
 + (id)createPermissionStrategy:(PermissionGroup)permission {
     switch (permission) {
-            case PermissionGroupCalendar:
-            return [EventPermissionStrategy new];
             case PermissionGroupCamera:
             return [AudioVideoPermissionStrategy new];
             case PermissionGroupContacts:
@@ -85,20 +83,8 @@
             case PermissionGroupLocationAlways:
             case PermissionGroupLocationWhenInUse:
             return [[LocationPermissionStrategy alloc] initWithLocationManager];
-            case PermissionGroupMediaLibrary:
-            return [MediaLibraryPermissionStrategy new];
             case PermissionGroupMicrophone:
             return [AudioVideoPermissionStrategy new];
-            case PermissionGroupPhone:
-            return [PhonePermissionStrategy new];
-            case PermissionGroupPhotos:
-            return [PhotoPermissionStrategy new];
-            case PermissionGroupReminders:
-            return [EventPermissionStrategy new];
-            case PermissionGroupSensors:
-            return [SensorPermissionStrategy new];
-            case PermissionGroupSpeech:
-            return [SpeechPermissionStrategy new];
         default:
             return [UnknownPermissionStrategy new];
     }
